7f634330dc16b76b2e2cd5612f9a2fdaf2712593
Rick Goshen <rick.goshen@gmail.com>
2025-09-12 21:47:26 -0700

    Refactors scene rendering for 2D appearance
    Simplifies the scene rendering logic by configuring the two pyramids to appear as flat 2D triangles.
    
    This achieves a cleaner visual presentation and prepares the scene for further UI enhancements.
    
    Removes previous transformation code.

--- Source/SceneManager.cpp
+++ Source/SceneManager.cpp
@@ -4,7 +4,7 @@
 // manage the loading and rendering of 3D scenes
 //
 //  AUTHOR: Brian Battersby - SNHU Instructor / Computer Science
-//	Created for CS-330-Computational Graphics and Visualization, Nov. 1st, 2023
+//  Created for CS-330-Computational Graphics and Visualization, Nov. 1st, 2023
 ///////////////////////////////////////////////////////////////////////////////
 
 #include "SceneManager.h"
@@ -19,11 +19,11 @@
 // declaration of global variables and defines
 namespace
 {
-	const char* g_ModelName = "model";
-	const char* g_ColorValueName = "objectColor";
-	const char* g_TextureValueName = "objectTexture";
-	const char* g_UseTextureName = "bUseTexture";
-	const char* g_UseLightingName = "bUseLighting";
+    const char* g_ModelName = "model";
+    const char* g_ColorValueName = "objectColor";
+    const char* g_TextureValueName = "objectTexture";
+    const char* g_UseTextureName = "bUseTexture";
+    const char* g_UseLightingName = "bUseLighting";
 }
 
 /***********************************************************
@@ -31,10 +31,10 @@ namespace
  *
  *  The constructor for the class
  ***********************************************************/
-SceneManager::SceneManager(ShaderManager *pShaderManager)
+SceneManager::SceneManager(ShaderManager* pShaderManager)
 {
-	m_pShaderManager = pShaderManager;
-	m_basicMeshes = new ShapeMeshes();
+    m_pShaderManager = pShaderManager;
+    m_basicMeshes = new ShapeMeshes();
 }
 
 /***********************************************************
@@ -44,9 +44,9 @@ SceneManager::SceneManager(ShaderManager *pShaderManager)
  ***********************************************************/
 SceneManager::~SceneManager()
 {
-	m_pShaderManager = NULL;
-	delete m_basicMeshes;
-	m_basicMeshes = NULL;
+    m_pShaderManager = NULL;
+    delete m_basicMeshes;
+    m_basicMeshes = NULL;
 }
 
 /***********************************************************
@@ -56,37 +56,37 @@ SceneManager::~SceneManager()
  *  using the passed in transformation values.
  ***********************************************************/
 void SceneManager::SetTransformations(
-	glm::vec3 scaleXYZ,
-	float XrotationDegrees,
-	float YrotationDegrees,
-	float ZrotationDegrees,
-	glm::vec3 positionXYZ)
+    glm::vec3 scaleXYZ,
+    float XrotationDegrees,
+    float YrotationDegrees,
+    float ZrotationDegrees,
+    glm::vec3 positionXYZ)
 {
-	// variables for this method
-	glm::mat4 modelView;
-	glm::mat4 scale;
-	glm::mat4 rotationX;
-	glm::mat4 rotationY;
-	glm::mat4 rotationZ;
-	glm::mat4 translation;
+    // variables for this method
+    glm::mat4 modelView;
+    glm::mat4 scale;
+    glm::mat4 rotationX;
+    glm::mat4 rotationY;
+    glm::mat4 rotationZ;
+    glm::mat4 translation;
 
-	// set the scale value in the transform buffer
-	scale = glm::scale(scaleXYZ);
-	// set the rotation values in the transform buffer
-	rotationX = glm::rotate(glm::radians(XrotationDegrees), glm::vec3(1.0f, 0.0f, 0.0f));
-	rotationY = glm::rotate(glm::radians(YrotationDegrees), glm::vec3(0.0f, 1.0f, 0.0f));
-	rotationZ = glm::rotate(glm::radians(ZrotationDegrees), glm::vec3(0.0f, 0.0f, 1.0f));
-	// set the translation value in the transform buffer
-	translation = glm::translate(positionXYZ);
+    // set the scale value in the transform buffer
+    scale = glm::scale(scaleXYZ);
+    // set the rotation values in the transform buffer
+    rotationX = glm::rotate(glm::radians(XrotationDegrees), glm::vec3(1.0f, 0.0f, 0.0f));
+    rotationY = glm::rotate(glm::radians(YrotationDegrees), glm::vec3(0.0f, 1.0f, 0.0f));
+    rotationZ = glm::rotate(glm::radians(ZrotationDegrees), glm::vec3(0.0f, 0.0f, 1.0f));
+    // set the translation value in the transform buffer
+    translation = glm::translate(positionXYZ);
 
-	// matrix math to calculate the final model matrix
-	modelView = translation * rotationX * rotationY * rotationZ * scale;
+    // matrix math to calculate the final model matrix
+    modelView = translation * rotationX * rotationY * rotationZ * scale;
 
-	if (NULL != m_pShaderManager)
-	{
-		// pass the model matrix into the shader
-		m_pShaderManager->setMat4Value(g_ModelName, modelView);
-	}
+    if (NULL != m_pShaderManager)
+    {
+        // pass the model matrix into the shader
+        m_pShaderManager->setMat4Value(g_ModelName, modelView);
+    }
 }
 
 /***********************************************************
@@ -96,25 +96,25 @@ void SceneManager::SetTransformations(
  *  into the shader for the next draw command
  ***********************************************************/
 void SceneManager::SetShaderColor(
-	float redColorValue,
-	float greenColorValue,
-	float blueColorValue,
-	float alphaValue)
+    float redColorValue,
+    float greenColorValue,
+    float blueColorValue,
+    float alphaValue)
 {
-	// variables for this method
-	glm::vec4 currentColor;
+    // variables for this method
+    glm::vec4 currentColor;
 
-	currentColor.r = redColorValue;
-	currentColor.g = greenColorValue;
-	currentColor.b = blueColorValue;
-	currentColor.a = alphaValue;
+    currentColor.r = redColorValue;
+    currentColor.g = greenColorValue;
+    currentColor.b = blueColorValue;
+    currentColor.a = alphaValue;
 
-	if (NULL != m_pShaderManager)
-	{
-		// set the color values into the shader
-		m_pShaderManager->setIntValue(g_UseTextureName, false);
-		m_pShaderManager->setVec4Value(g_ColorValueName, currentColor);
-	}
+    if (NULL != m_pShaderManager)
+    {
+        // set the color values into the shader
+        m_pShaderManager->setIntValue(g_UseTextureName, false);
+        m_pShaderManager->setVec4Value(g_ColorValueName, currentColor);
+    }
 }
 
 /**************************************************************/
@@ -128,88 +128,60 @@ void SceneManager::SetShaderColor(
  *  PrepareScene()
  *
  *  This method is used for preparing the 3D scene by loading
- *  the shapes, textures in memory to support the 3D scene 
+ *  the shapes, textures in memory to support the 3D scene
  *  rendering
  ***********************************************************/
 void SceneManager::PrepareScene()
 {
-	// only one instance of a particular mesh needs to be
-	// loaded in memory no matter how many times it is drawn
-	// in the rendered 3D scene
-
-	m_basicMeshes->LoadPyramid4Mesh();
+    // only one instance of a particular mesh needs to be
+    // loaded in memory no matter how many times it is drawn
+    // in the rendered 3D scene
+    m_basicMeshes->LoadPyramid4Mesh();
 }
 
 /***********************************************************
  *  RenderScene()
  *
- *  This method is used for rendering the 3D scene by 
+ *  This method is used for rendering the 3D scene by
  *  transforming and drawing the basic 3D shapes
  ***********************************************************/
 void SceneManager::RenderScene()
 {
-	// declare the variables for the transformations
-	glm::vec3 scaleXYZ;
-	float XrotationDegrees = 0.0f;
-	float YrotationDegrees = 0.0f;
-	float ZrotationDegrees = 0.0f;
-	glm::vec3 positionXYZ;
+    // ---------- controls ----------
+    const float  scaleXY = 2.40f;   // overall triangle size (uniform X/Y)
+    const float  centerShiftY = 0.00f;   // move both triangles up/down together
+    // ------------------------------
 
-	/*** Set needed transformations before drawing the basic mesh.  ***/
-	/*** This same ordering of code should be used for transforming ***/
-	/*** and drawing all the basic 3D shapes.						***/
-	/******************************************************************/
-	// set the XYZ scale for the mesh for top pyramid
-	scaleXYZ = glm::vec3(1.0f, 1.0f, 1.0f);
+    // Make the pyramid effectively 2D so perspective can't skew sizes
+    const glm::vec3 kScale = glm::vec3(scaleXY, scaleXY, 0.001f); // <- almost zero depth
+    const float     kYOffset = 0.48f * scaleXY;   // tips meet cleanly without overlap
+    const float     kFlipZDeg = 180.0f;            // bottom triangle points down
 
-	// set the XYZ rotation for the mesh for top pyramid
-	XrotationDegrees = 180.0f;
-	YrotationDegrees = 90.0f;
-	ZrotationDegrees = 0.0f;
+    // solid white, no texture
+    SetShaderColor(1.0f, 1.0f, 1.0f, 1.0f);
 
-	// set the XYZ position for the mesh for top pyramid
-	positionXYZ = glm::vec3(0.0f, 0.5f, 0.0f);
+    // -------- TOP triangle (upright) --------
+    SetTransformations(
+        /*scale*/    kScale,
+        /*rotX*/     0.0f,
+        /*rotY*/     0.0f,
+        /*rotZ*/     0.0f,
+        /*position*/ glm::vec3(0.0f, -kYOffset + centerShiftY, 0.0f)
+    );
+    m_basicMeshes->DrawPyramid4Mesh();
 
-	// set the transformations into memory to be used on the drawn meshes
-	SetTransformations(
-		scaleXYZ,
-		XrotationDegrees,
-		YrotationDegrees,
-		ZrotationDegrees,
-		positionXYZ);
-
-	SetShaderColor(1, 1, 1, 1);
-
-	// draw the mesh with transformation values for bottom pyramid
-	m_basicMeshes->DrawPyramid4Mesh();
-	/****************************************************************/
-
-	/*** Set needed transformations before drawing the basic mesh.  ***/
-	/*** This same ordering of code should be used for transforming ***/
-	/*** and drawing all the basic 3D shapes.						***/
-	/******************************************************************/
-	// set the XYZ scale for the mesh for bottom pyramid
-	scaleXYZ = glm::vec3(1.0f, 1.0f, 1.0f);
-
-	// set the XYZ rotation for the mesh for bottom pyramid
-	XrotationDegrees = 0.0f;
-	YrotationDegrees = 90.0f;
-	ZrotationDegrees = -30.0f;
-
-	// set the XYZ position for the mesh for bottom pyramid
-	positionXYZ = glm::vec3(0.0f, -0.5f, 0.0f);
-
-	// set the transformations into memory to be used on the drawn meshes
-	SetTransformations(
-		scaleXYZ,
-		XrotationDegrees,
-		YrotationDegrees,
-		ZrotationDegrees,
-		positionXYZ);
-
-	SetShaderColor(1, 1, 1, 1);
-
-	// draw the mesh with transformation values
-	m_basicMeshes->DrawPyramid4Mesh();
-	/****************************************************************/
+    // -------- BOTTOM triangle (flipped 180° around Z) --------
+    SetTransformations(
+        /*scale*/    kScale,
+        /*rotX*/     0.0f,
+        /*rotY*/     0.0f,
+        /*rotZ*/     kFlipZDeg,
+        /*position*/ glm::vec3(0.0f, +kYOffset + centerShiftY, 0.0f)
+    );
+    m_basicMeshes->DrawPyramid4Mesh();
 }
+
+
+
+
+
